bd3b2dfc47f16cc8a32738ea0dbc7ce4a455e643
Leonam <148882032+Leonamexe@users.noreply.github.com>
2024-06-08 08:26:55 -0300

    Add files via upload

---
+++ Testando JS/main.js
@@ -0,0 +1,84 @@
+//alert("Hello World!");
+
+
+//var numero1 = 5;
+//var numero2 = 3;
+//var soma = numero1 + numero2;
+//console.log("O resultado da soma é: " + soma);
+//alert("O resultado da soma é: " + soma);
+
+
+//var valor = 10;
+
+//if (typeof valor === "number") {
+  //  console.log("É um número");
+  //  alert("É um número");
+//} else {
+  //  console.log("Não é um número");
+    //alert("Não é um número");
+//}
+
+
+//var valor = "Hello";
+
+//if (typeof valor === "string") {
+   // console.log("É uma string");
+   // alert("É uma string");
+//} else {
+   // console.log("Não é uma string");
+   // alert("Não é uma string");
+//}
+
+
+//var valor = true;
+
+//if (typeof valor === "boolean") {
+//    console.log("É um booleano");
+//    alert("É um booleano");
+//} else {
+//    console.log("Não é um booleano");
+//    alert("Não é um booleano");
+//}
+
+
+//var numero1 = 8;
+//var numero2 = 3;
+//var subtracao = numero1 - numero2;
+//console.log("O resultado da subtração é: " + subtracao);
+//alert("O resultado da subtração é: " + subtracao);
+
+
+//var numero1 = 4;
+//var numero2 = 7;
+//var multiplicacao = numero1 * numero2;
+//console.log("O resultado da multiplicação é: " + multiplicacao);
+//alert("O resultado da multiplicação é: " + multiplicacao);
+
+
+//var numero1 = 20;
+//var numero2 = 4;
+//var divisao = numero1 / numero2;
+//console.log("O resultado da divisão é: " + divisao);
+//alert("O resultado da divisão é: " + divisao);
+
+
+//var valor = 6;
+
+//if (typeof valor === "number" && valor % 2 === 0) {
+//    console.log("É um número par");
+//    alert("É um número par");
+//} else {
+//   console.log("Não é um número par");
+//    alert("Não é um número par");
+//}
+
+
+var valor = 7;
+
+if (typeof valor === "number" && valor % 2 !== 0) {
+    console.log("É um número ímpar");
+    alert("É um número ímpar");
+} else {
+    console.log("Não é um número ímpar");
+    alert("Não é um número ímpar");
+}
